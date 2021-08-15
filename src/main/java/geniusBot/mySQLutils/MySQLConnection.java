2b3cf499561138742b9b372c9de93ed0c6174993
Gabor Szita <gaborszita@yahoo.com>
2021-08-15 08:55:08 -0700

    Use one connection per query instead of persistent connections.

--- src/main/java/geniusBot/mySQLutils/MySQLConnection.java
+++ src/main/java/geniusBot/mySQLutils/MySQLConnection.java
@@ -23,39 +23,16 @@ import java.sql.*;
 
 public class MySQLConnection
 {
-   private static boolean isConnected = false;
-   private static Connection con;
-   
    public static Connection getConnection() throws SQLException
    {
-      if(isConnected)
-      {
-         return con;
-      }
-      else
-      {
-         Connect();
-         return con;
-      }
+      return Connect();
    }
    
-   public static void Connect() throws SQLException
+   public static Connection Connect() throws SQLException
    {
-      if(!isConnected)
-      {
-         con = DriverManager.getConnection("jdbc:mysql://localhost:3306/"
-               + "dbname?useSSL=false&serverTimezone=UTC", "dbusername",
-               "dbpassword");
-      }
-      isConnected = true;
-   }
-   
-   public static void Disconnect() throws SQLException
-   {
-      if(isConnected)
-      {
-         con.close();
-      }
-      isConnected = false;
+      Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/"
+            + "dbname?useSSL=false&serverTimezone=UTC", "dbusername",
+            "dbpassword");
+      return con;
    }
 }
